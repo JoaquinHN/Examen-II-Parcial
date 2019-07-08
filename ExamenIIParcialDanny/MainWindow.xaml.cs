4fce698e43b94723a0c16999541417bcf2e953bb
DannyJ <dannycantarero3@gmail.com>
2019-07-08 13:08:18 -0600

    Disenio terminado y cambios a los botones

--- ExamenIIParcialDanny/MainWindow.xaml.cs
+++ ExamenIIParcialDanny/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
 namespace ExamenIIParcialDanny
 {
     /// <summary>
@@ -20,9 +22,47 @@ namespace ExamenIIParcialDanny
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        SqlConnection sqlconnection;
+
         public MainWindow()
         {
+            string connectionString = ConfigurationManager.ConnectionStrings["ExamenIIParcialDanny.Properties.Settings.ERPConnectionString"].ConnectionString;
+            sqlconnection = new SqlConnection(connectionString);
             InitializeComponent();
+
+
+        }
+
+        private void btnGuardar(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string query = "INSERT INTO Usuarios.usuario(nombre,apellido,nombreUsuario,contrasenia,correo,tipoUsuario) VALUES(@nombre,@apellido,@nombreUs,@contrasenia,@correo,@tipoUsuario)";
+                SqlCommand sqlCommand = new SqlCommand(query, sqlconnection);
+                //Abrir conexion
+                sqlconnection.Open();
+                //remplazar el parametro con su valor respectivo
+                sqlCommand.Parameters.AddWithValue("@nombre", txtnombre.Text);
+                sqlCommand.Parameters.AddWithValue("@apellido", txtapellido.Text);
+                sqlCommand.Parameters.AddWithValue("@nombre", txtnombre.Text);
+                //Ejecutamos query de insersion
+                sqlCommand.ExecuteScalar();
+                //Limpiar el valor del texto en txtinformacion
+                txtInformacion.Text = string.Empty;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                sqlconnection.Close();
+                //Actualizar el Listbox de zoologicos
+                MostrarZoologicos();
+            }
         }
     }
 }
